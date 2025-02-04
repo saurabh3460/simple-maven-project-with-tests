abc9bc48a67750e5de0ef4dd055f4ad754ee6e9b
Saurabh <30365494+saurabh3460@users.noreply.github.com>
2025-02-04 12:09:00 +0530

    update

--- src/test/java/test/Base.java
+++ src/test/java/test/Base.java
@@ -30,9 +30,9 @@ import org.junit.internal.AssumptionViolatedException;
 class Base {
 
     protected void run() {
-        double r = Math.random();
-        // double r = 0.0;
-        if (r < 0.1 {
+        // double r = Math.random();
+        double r = 0.0;
+        if (r < 0.1) {
             fail("oops");
         } else if (r < 0.2) {
             throw new AssumptionViolatedException("skipping");
